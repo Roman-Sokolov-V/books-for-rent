947af90fc35d7d2c704844f25609ab5620c10ce3
Roman Sokolov <gnonasis@gmail.com>
2025-02-21 14:32:53 +0200

    add run schedule task with runbot

--- telegram_bot/management/commands/runbot.py
+++ telegram_bot/management/commands/runbot.py
@@ -1,9 +1,14 @@
 from django.core.management.base import BaseCommand
 from telegram_bot.bot import start_bot
+from telegram_bot.tasks import create_scheduled_task
 
 class Command(BaseCommand):
     help = 'Запускає Telegram бота'
 
     def handle(self, *args, **options):
         self.stdout.write("Запуск Telegram бота...")
+        create_scheduled_task()
+        print("scheduled_task starts")
         start_bot()
+        print("bot is running")
+
