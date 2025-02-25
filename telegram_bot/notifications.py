8d8e892f604a802c978cf7ef82741287748b0a18
Roman Sokolov <gnonasis@gmail.com>
2025-02-25 14:54:33 +0200

    change notifications.py no work with aiogram

--- telegram_bot/notifications.py
+++ telegram_bot/notifications.py
@@ -1,43 +1,51 @@
+import asyncio
+from aiogram import Bot
+
 from django.utils import timezone
-import telebot
-from django_q.tasks import async_task
-import logging
+
 
 from books_rent_config.settings import TELEGRAM_BOT_TOKEN
-
 from borrowing.models import Borrowing
 
-logger = logging.getLogger(__name__)
 
-bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
+bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
-def send_created(user_id: int, borrowing: Borrowing) -> None:
+async def send_created(user_id: int=674340411, borrowing: Borrowing=None) -> None:
     rented_days = (timezone.now().date() - borrowing.borrow_date).days
-    bot.send_message(
+    await bot.send_message(
         user_id,
         f"You rented a {borrowing.book.title} on {borrowing.borrow_date}, the cost of rental is $ {borrowing.book.daily_fee} a day, today accrued the cost of rolling {borrowing.book.daily_fee * rented_days} $"
     )
+    await bot.session.close()
 
-def message_expired(user_id: int, book_title: str, expired_days: int) -> None:
-    bot.send_message(
+def run_send_created(user_id: int, borrowing: Borrowing) -> None:
+    asyncio.run(send_created(user_id, borrowing))
+
+
+async def message_expired(user_id: int=674340411, book_title: str="test", expired_days: int=1) -> None:
+    await bot.send_message(
         user_id,
         f"Your rented book {book_title} is overdue for {expired_days}, please return the book as soon as possible"
     )
+    await bot.session.close()
     print("Message (expired book) is sent")
 
-def find_expired_and_send_message() -> None:
+
+def run_messages_expired() -> None:
     borrowings = Borrowing.objects.filter(expected_return_date__lt=timezone.now()).select_related("user", "book")
     for borrowing in borrowings:
         expired_days = (timezone.now().date() - borrowing.expected_return_date).days
         if borrowing.user.telegram_id:
             user_id = borrowing.user.telegram_id
-
-            async_task(
-                "telegram_bot.notifications.message_expired",
-                user_id=user_id,
-                book_title=borrowing.book.title,
-                expired_days=expired_days,
+            asyncio.run(
+                (
+                    message_expired(
+                        user_id=user_id,
+                        book_title=borrowing.book.title,
+                        expired_days=expired_days
+                    )
+                )
             )
         else:
-            logger.warning(f"The user {borrowing.user.id} has an overdue book but does not have telegram_id")
+            print(f"user {borrowing.user} - {borrowing.user.email}has not had telegram id yet")
 
