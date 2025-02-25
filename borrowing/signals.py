2e7b159b319f69bcfc480b52c1ec6f71aa900fc6
Roman Sokolov <gnonasis@gmail.com>
2025-02-25 14:55:36 +0200

    change signals.py to work with aiogram

--- borrowing/signals.py
+++ borrowing/signals.py
@@ -1,19 +1,11 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from borrowing.models import Borrowing
-from telegram_bot.notifications import send_created
+from telegram_bot.notifications import run_send_created
 
 @receiver(post_save, sender=Borrowing)
 def borrowing_created(sender, instance, created, **kwargs):
     if created:
-        print(f"Borrowing '{instance.id}' by {instance.book} has been created.")
         user_id = instance.user.telegram_id
-        send_created(user_id, instance)
-
-
-# @receiver(post_save, sender=Borrowing)
-# def paid_success(sender, instance, created, **kwargs):
-#     if created:
-#         print(f"Borrowing '{instance.id}' by {instance.book} has been created.")
-#         user_id = instance.user.telegram_id
-#         send_created(user_id, instance)
+        run_send_created(user_id=user_id, borrowing=instance)
+        print(f"Borrowing '{instance.id}' by {instance.book} has been created.")
