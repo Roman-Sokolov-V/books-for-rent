708c68dea7b04eaaeb05642254adb85131bdcb13
Roman Sokolov <gnonasis@gmail.com>
2025-02-25 14:55:48 +0200

    change tasks.py to work with aiogram

--- telegram_bot/tasks.py
+++ telegram_bot/tasks.py
@@ -1,26 +1,21 @@
-from django_q.tasks import schedule
 from django_q.models import Schedule
+from django_q.tasks import async_task
 from django.utils import timezone
 from datetime import timedelta
 
-
-# schedule(
-#     "telegram_bot.notifications.find_expired_and_send_message",
-#     schedule_type='M',
-#     next_run=timezone.localtime() + timedelta(minutes=1),
-#     repeats=-1,
-#)
 ##  Не забути запускати django-q   python manage.py qcluster
-
-def print_report(*args):
-    print(f"schedule {args} created")
 
 
 def create_scheduled_task():
     Schedule.objects.create(
-        func='telegram_bot.notifications.find_expired_and_send_message',
-        hook='telegram_bot.tasks.print_report',
-        next_run=timezone.localtime().now() + timedelta(seconds=30),
-        schedule_type=Schedule.MINUTES,
-        repeats=3,
+        func='telegram_bot.tasks.find_expired_and_send_message',
+        next_run=timezone.now() + timedelta(seconds=30),
+        schedule_type=Schedule.DAILY,
+        repeats=-1,
         )
+
+
+def find_expired_and_send_message() -> None:
+    async_task(
+        "telegram_bot.notifications.run_messages_expired",
+    )
