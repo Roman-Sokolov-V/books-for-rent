d6a7567b0f8d6571a7d12216a73927a9a82fb522
Roman Sokolov <gnonasis@gmail.com>
2025-02-25 14:55:08 +0200

    change runbot.py to work with aiogram

--- telegram_bot/management/commands/runbot.py
+++ telegram_bot/management/commands/runbot.py
@@ -1,5 +1,6 @@
+import asyncio
 from django.core.management.base import BaseCommand
-from telegram_bot.bot import start_bot
+from telegram_bot.bot import main
 from telegram_bot.tasks import create_scheduled_task
 
 class Command(BaseCommand):
@@ -9,6 +10,5 @@ class Command(BaseCommand):
         self.stdout.write("Запуск Telegram бота...")
         create_scheduled_task()
         print("scheduled_task starts")
-        start_bot()
+        asyncio.run(main())
         print("bot is running")
-
